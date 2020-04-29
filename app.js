8f216435de3e7480cd7b940566b79560f90bd89e
Gamygams1234 <gamyburgos@gmail.com>
2020-04-28 17:42:02 -0700

    Create a React Element

---
+++ app.js
@@ -0,0 +1,5 @@
+// create element has three arguments tag, properties, then the children
+const title = React.creatElement("h1", { id: "main-title", title: "This is a title" }, "my react element");
+
+// the type is H1
+// this is an object that we have to render to the dom
