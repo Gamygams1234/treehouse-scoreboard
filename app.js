24938964a5fc078685cbbaf72406ab5f52f5521b
Gamygams1234 <gamyburgos@gmail.com>
2020-04-28 18:58:21 -0700

    Create a Component

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-// React.createElement("h1", { id: "main-title", title: "This is a title" }, "my react element")
-const name = "Gamy";
-// React.createElement("p", null, "I am Gay, I am broke as shit, Carole Baskins killed her husband");
-const desc = <p>I am Gay, I am broke as shit, Carole Baskins killed her husband</p>;
-const myTitleID = "main-title";
-// here we can pass all of the information on the header element
-const header = (
-  <header>
-    <h1 id={myTitleID}>{name}'s React Element</h1>
-    {desc}
-  </header>
-);
+function Header() {
+  return (
+    <header>
+      <h1>Scoereboard</h1>
+      <span className="stats">Players:1</span>
+    </header>
+  );
+}
 
 // with the compiler, these elements get rendered to the DOM uding JSX
 ReactDOM.render(header, document.getElementById("root"));
