8d8716431fc6c4bc8de1fdb435c0bf022e80e6e1
Gamygams1234 <gamyburgos@gmail.com>
2020-04-28 19:18:31 -0700

    Composing Components

--- app.js
+++ app.js
@@ -17,11 +17,17 @@ const Player = () => {
   return (
     <div className="player">
       <span className="player-name">Gamy</span>
-      <div className="counter">
-        <button className="counter-action decrement">-</button>
-        <span className="counter-score">35</span>
-        <button className="counter-action increment">+</button>
-      </div>
+      <Counter />
+    </div>
+  );
+};
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement">-</button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment">+</button>
     </div>
   );
 };
