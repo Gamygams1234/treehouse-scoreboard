1f8d14cef432ebbd1e74b729ecadd55f35c1539a
Gamygams1234 <gamyburgos@gmail.com>
2020-04-29 12:12:23 -0700

    Updating State

--- app.js
+++ app.js
@@ -28,13 +28,22 @@ class Counter extends React.Component {
       score: 0,
     };
   }
+  decrementScore = () => {
+    this.setState({
+      score: this.state.score - 1,
+    });
+  };
   incrementScore = () => {
-    console.log("hello");
+    this.setState({
+      score: this.state.score + 1,
+    });
   };
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement">-</button>
+        <button className="counter-action decrement" onClick={this.decrementScore}>
+          -
+        </button>
         <span className="counter-score">{this.state.score}</span>
         <button className="counter-action increment" onClick={this.incrementScore}>
           +
