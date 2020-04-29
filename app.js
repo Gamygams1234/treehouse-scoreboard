dc1dc1714ad1346c52a76ca283decfa153a2a8da
Gamygams1234 <gamyburgos@gmail.com>
2020-04-28 19:07:16 -0700

    Components as Arrow Functions

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-function Header() {
+// you can write an impicit returm by omitting curly braces and the return keyword
+// const Header = () => (
+//     <header>
+//       <h1>Scoereboard</h1>
+//       <span className="stats">Players:1</span>
+//     </header>
+//   );
+const Header = () => {
   return (
     <header>
       <h1>Scoereboard</h1>
       <span className="stats">Players:1</span>
     </header>
   );
-}
+};
 
 // with the compiler, these elements get rendered to the DOM uding JSX
-ReactDOM.render(header, document.getElementById("root"));
+ReactDOM.render(<Header />, document.getElementById("root"));
