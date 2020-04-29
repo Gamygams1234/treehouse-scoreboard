6575bb5f6f9431ff38ab26e3f5db50efe5455a3d
Gamygams1234 <gamyburgos@gmail.com>
2020-04-28 17:53:21 -0700

    Render an Element

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // create element has three arguments tag, properties, then the children
-const title = React.creatElement("h1", { id: "main-title", title: "This is a title" }, "my react element");
+const title = React.createElement("h1", { id: "main-title", title: "This is a title" }, "my react element");
+const desc = React.createElement("p", null, "I am Gay, I am broke as shit, Carole Baskins killed her husband");
+// here we can pass all of the information on the header element
+const header = React.createElement("header", null, title, desc);
 
-// the type is H1
-// this is an object that we have to render to the dom
+// this has two arguments,(the element we want to render, and the HTML element we want to Update)
+// reacts jus manages what gets rendered to the DOM
+ReactDOM.render(header, document.getElementById("root"));
